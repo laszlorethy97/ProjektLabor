26c5d54137525a69cc0c88b1cec279f38aca9d64
V0ida <v0ida@Kalx.dsl.pool.telekom.hu>
2026-09-12 21:22:20 +0200

    feat: DbContext letrehozasa es adatbazis kapcsolatok megirasa

---
+++ backend/Data/KeyManagementDbContext.cs
@@ -0,0 +1,104 @@
+using Microsoft.EntityFrameworkCore;
+using KeyManagement.Api.Models.Entities;
+
+namespace KeyManagement.Api.Data
+{
+public class KeyManagementDbContext : DbContext
+    {
+    public KeyManagementDbContext(DbContextOptions<KeyManagementDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Building> Buildings { get; set; }
+    public DbSet<Key> Keys { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<KeyTransaction> KeyTransactions { get; set; }
+    public DbSet<AuditLog> AuditLogs { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<Equipment> Equipments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Building>()
+            .HasMany(r => r.Rooms)
+            .WithOne(b => b.Building)
+            .HasForeignKey(r => r.BuildingId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Room>()
+            .HasMany(e => e.Equipments)
+            .WithOne(r => r.Room)
+            .HasForeignKey(e => e.RoomId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Room>()
+            .HasMany(r => r.Reservations)
+            .WithOne(r => r.Room)
+            .HasForeignKey(e => e.RoomId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Room>()
+            .HasMany(k => k.Keys)
+            .WithOne(r => r.Room)
+            .HasForeignKey(e => e.RoomId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.UserRoles)
+            .WithOne(r => r.User)
+            .HasForeignKey(e => e.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.Requester)
+            .WithMany()
+            .HasForeignKey(r => r.RequesterId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.Approver)
+            .WithMany()
+            .HasForeignKey(r => r.ApproverId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+       modelBuilder.Entity<KeyTransaction>()
+            .HasOne(kt => kt.HolderUser)
+            .WithMany()
+            .HasForeignKey(kt => kt.HolderUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<KeyTransaction>()
+            .HasOne(kt => kt.IssuedByUser)
+            .WithMany()
+            .HasForeignKey(kt => kt.IssuedByUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<KeyTransaction>()
+            .HasOne(kt => kt.ReturnedToUser)
+            .WithMany()
+            .HasForeignKey(kt => kt.ReturnedToUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Role>()
+            .HasMany(r => r.UserRoles)
+            .WithOne(ur => ur.Role)
+            .HasForeignKey(ur => ur.RoleId)
+            .OnDelete(DeleteBehavior.Restrict);ű
+
+        modelBuilder.Entity<UserRole>()
+            .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+
+
+
+
+
+
+        
+
+    }
+    }
+}
