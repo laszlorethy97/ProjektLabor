a47f79662c14a97b2fac9df2033141a1ad72a953
V0ida <v0ida@Kalx.dsl.pool.telekom.hu>
2026-09-15 16:57:28 +0200

    feat: Ticket osztaly letrehozasa AuditLog helyett+Userrel kapcsolat

--- backend/Models/Entities/User.cs
+++ backend/Models/Entities/User.cs
@@ -6,20 +6,17 @@ namespace KeyManagement.Api.Models.Entities
     {
         
         public int Id { get; set; }
-        [StringLength(50)]
-        public string Name { get; set; } = string.Empty;
+        
         [StringLength(256)]
         public string Email { get; set; } = string.Empty;
         [StringLength(256)]
         public string PasswordHash{ get; set; } = string.Empty;
         [StringLength(6)]
         public string PinCode { get; set; } = string.Empty;
-        [StringLength(15)]
-
-        public bool IsActive { get; set; } = true;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-        public int CreatedById { get; set; }
+        public int? CreatedById { get; set; }
+        public User? CreatedBy { get; set; }
 
         public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
         public ICollection<Permission> Permissions { get; set; } = new List<Permission>();
@@ -28,7 +25,6 @@ namespace KeyManagement.Api.Models.Entities
         public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
         public ICollection<Maintenance> Maintenances { get; set; } = new List<Maintenance>();
         public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
-        
 
     }
 }
