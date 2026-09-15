c0ae577c731c2f416d7d4225d29b39db137f0269
V0ida <v0ida@Kalx.dsl.pool.telekom.hu>
2026-09-15 16:41:41 +0200

    fix: Reservation osztaly javitasa

--- backend/Models/Entities/Reservation.cs
+++ backend/Models/Entities/Reservation.cs
@@ -7,20 +7,11 @@ namespace KeyManagement.Api.Models.Entities
     {
         
         public int Id { get; set; }
-        public int RoomId { get; set; }
-        public int RequesterId { get; set; }
-        public int? ApproverId { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        public DateTime? ReviewedAt { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        [StringLength(500)]
-        public string? RejectionReason { get; set; } = null;
-        public ReservationState State { get; set; } 
         public Room Room { get; set; } = null!;
-        public User Requester { get; set; } = null!;
-        public User? Approver { get; set; } 
-        public ICollection<KeyTransaction> KeyTransactions { get; set; } = new List<KeyTransaction>();
-
+        public User User { get; set; } = null!;
+        public ICollection<Equipment> Equipments { get; set; } = new List<Equipment>();
     }
 }
