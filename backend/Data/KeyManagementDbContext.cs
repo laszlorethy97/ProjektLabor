3a6b0b7acfe63e8c4665db51560e06c2e0b766c9
V0ida <v0ida@Kalx.dsl.pool.telekom.hu>
2026-09-15 17:40:37 +0200

    feat: DbContext modositasa az uj db layout alapjan

--- backend/Data/KeyManagementDbContext.cs
+++ backend/Data/KeyManagementDbContext.cs
@@ -3,101 +3,165 @@ using KeyManagement.Api.Models.Entities;
 
 namespace KeyManagement.Api.Data
 {
-public class KeyManagementDbContext : DbContext
+    public class KeyManagementDbContext : DbContext
     {
-    public KeyManagementDbContext(DbContextOptions<KeyManagementDbContext> options) : base(options)
-    {
-    }
+        public KeyManagementDbContext(DbContextOptions<KeyManagementDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<Key> Keys { get; set; }
+        public DbSet<KeyTransaction> KeyTransactions { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Maintenance> Maintenances { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<MasterKey> MasterKeys { get; set; }
+        public DbSet<MasterKeyTransaction> MasterKeyTransactions { get; set; }
 
-    public DbSet<Building> Buildings { get; set; }
-    public DbSet<Key> Keys { get; set; }
-    public DbSet<Reservation> Reservations { get; set; }
-    public DbSet<KeyTransaction> KeyTransactions { get; set; }
-    public DbSet<AuditLog> AuditLogs { get; set; }
-    public DbSet<Room> Rooms { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Role> Roles { get; set; }
-    public DbSet<UserRole> UserRoles { get; set; }
-    public DbSet<Equipment> Equipments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Building>()
-            .HasMany(r => r.Rooms)
-            .WithOne(b => b.Building)
-            .HasForeignKey(r => r.BuildingId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Room>()
+                .HasOne(r => r.Building)
+                .WithMany(b => b.Rooms)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<Room>()
-            .HasMany(r => r.Equipments)
-            .WithOne(e => e.Room)
-            .HasForeignKey(e => e.RoomId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Equipment>()
+                .HasOne(e => e.Room)
+                .WithMany(r => r.Equipments)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<Room>()
-            .HasMany(r => r.Reservations)
-            .WithOne(r => r.Room)
-            .HasForeignKey(r => r.RoomId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Equipment>()
+                .HasOne(e => e.CreatedBy)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<Room>()
-            .HasMany(k => k.Keys)
-            .WithOne(r => r.Room)
-            .HasForeignKey(k => k.RoomId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Key>()
+                .HasOne(k => k.Room)
+                .WithMany(r => r.Keys)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<User>()
-            .HasMany(u => u.UserRoles)
-            .WithOne(r => r.User)
-            .HasForeignKey(u => u.UserId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Key>()
+                .HasOne(k => k.CreatedBy)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<Reservation>()
-            .HasOne(r => r.Requester)
-            .WithMany()
-            .HasForeignKey(r => r.RequesterId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.CreatedBy)
+                .WithMany()
+                .HasForeignKey(u => u.CreatedById)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<Reservation>()
-            .HasOne(r => r.Approver)
-            .WithMany()
-            .HasForeignKey(r => r.ApproverId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .OnDelete(DeleteBehavior.Restrict);
 
-       modelBuilder.Entity<KeyTransaction>()
-            .HasOne(kt => kt.HolderUser)
-            .WithMany()
-            .HasForeignKey(kt => kt.HolderUserId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<KeyTransaction>()
-            .HasOne(kt => kt.IssuedByUser)
-            .WithMany()
-            .HasForeignKey(kt => kt.IssuedByUserId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.CreatedBy)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<KeyTransaction>()
-            .HasOne(kt => kt.ReturnedToUser)
-            .WithMany()
-            .HasForeignKey(kt => kt.ReturnedToUserId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Permission>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Permissions)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<Role>()
-            .HasMany(r => r.UserRoles)
-            .WithOne(ur => ur.Role)
-            .HasForeignKey(r => r.RoleId)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Permission>()
+                .HasOne(p => p.CreatedBy)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<UserRole>()
-            .HasKey(ur => new { ur.UserId, ur.RoleId });
+            modelBuilder.Entity<Permission>()
+                .HasOne(p => p.MasterKey)
+                .WithMany(mk => mk.Permissions)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<AuditLog>()
-            .HasOne(al => al.User)
-            .WithMany(u => u.AuditLogs)
-            .HasForeignKey(al => al.UserId)
-            .IsRequired(false)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<MasterKey>()
+                .HasOne(mk => mk.CreatedBy)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
 
-    }
+            modelBuilder.Entity<KeyTransaction>()
+                .HasOne(kt => kt.Key)
+                .WithMany(k => k.KeyTransactions)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<KeyTransaction>()
+                .HasOne(kt => kt.User)
+                .WithMany(u => u.KeyTransactions)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<KeyTransaction>()
+                .HasOne(kt => kt.StartedByUser)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<KeyTransaction>()
+                .HasOne(kt => kt.EndedByUser)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<MasterKeyTransaction>()
+                .HasOne(mkt => mkt.MasterKey)
+                .WithMany(mk => mk.MasterKeyTransactions)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<MasterKeyTransaction>()
+                .HasOne(mkt => mkt.User)
+                .WithMany(u => u.MasterKeyTransactions)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<MasterKeyTransaction>()
+                .HasOne(mkt => mkt.StartedByUser)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<MasterKeyTransaction>()
+                .HasOne(mkt => mkt.EndedByUser)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<MasterKeyTransaction>()
+                .HasOne(mkt => mkt.ApprovedByUser)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.Room)
+                .WithMany(rm => rm.Reservations)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.User)
+                .WithMany(u => u.Reservations)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Maintenance>()
+                .HasOne(m => m.Room)
+                .WithMany(r => r.Maintenances)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Maintenance>()
+                .HasOne(m => m.User)
+                .WithMany(u => u.Maintenances)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Ticket>()
+                .HasOne(t => t.CreatedBy)
+                .WithMany(u => u.Tickets)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
