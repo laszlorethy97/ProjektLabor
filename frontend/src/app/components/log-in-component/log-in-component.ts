9d9fcc0ee7e8be3c58fd6fab2106684d72351477
lrethy <laszlorethy97@gmail.com>
2026-09-21 16:57:07 +0200

    feat a login adatainak ellenörzése

--- frontend/src/app/components/log-in-component/log-in-component.ts
+++ frontend/src/app/components/log-in-component/log-in-component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LogInService } from '../../services/log-in-service';
+import { LoginDto } from '../../dtos/login-dto';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './log-in-component.html',
   styleUrl: './log-in-component.scss',
 })
-export class LogInComponent {}
+export class LogInComponent {
+  constructor(
+    private readonly router: Router,
+    private readonly loginService: LogInService,
+  ){}
+
+  login(userForm: NgForm){
+    const dto: LoginDto = userForm.value as unknown as LoginDto
+    this.loginService.login(dto).subscribe({
+      next: (res) => {
+        console.log(res)
+      },
+      error: (err) =>{
+        alert("Hibás bejelentkezés történt")
+      }
+    });
+  }
+}
